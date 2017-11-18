dd3b02ff08d7bbcd9f250c5c335b6ad523870368
Simon <zachau@in.tum.de>
2017-11-18 15:07:27 +0100

    added hide irrelevant points

--- magic.js
+++ magic.js
@@ -1,66 +1,71 @@
 var points = [
-    [
-        [480, 200],
-        [580, 400],
-        [680, 100]
-    ],
-    [
-        [680, 100],
-        [780, 300],
-        [180, 300]
-    ],
-    [
-        [180, 300],
-        [280, 100],
-        [480, 200]
-    ]
+	[
+		[480, 200],
+		[580, 400],
+		[680, 100]
+	],
+	[
+		[680, 100],
+		[780, 300],
+		[180, 300]
+	],
+	[
+		[180, 300],
+		[280, 100],
+		[480, 200]
+	]
 ];
 
 var svg = d3.select("body").append("svg")
-    .attr("width", 960)
-    .attr("height", 500);
+	.attr("width", 960)
+	.attr("height", 500);
 
 var paths = points.map(points =>
-    svg.append("path")
-        .data([points])
-        .attr("d", d3.svg.line()
-            .tension(0) // Catmull–Rom
-            .interpolate("cardinal")
-        )
+	svg.append("path")
+		.data([points])
+		.attr("d", d3.svg.line()
+			.tension(0) // Catmull–Rom
+			.interpolate("cardinal")
+		)
 );
 
 points.forEach(points => {
-    svg.selectAll(".point")
-        .data(points)
-        .enter().append("circle")
-        .attr("r", 4)
-        .attr("transform", function(d) { return "translate(" + d + ")"; });
+	svg.selectAll(".point")
+		.data(points)
+		.enter().append("circle")
+		.attr("transform", function(d) { 
+			return "translate(" + d + ")"; 
+		})
+		.filter(function(d, i) {
+			return i == 0;
+		})
+		.attr("r", 4);
 });
 
 var circle = svg.append("circle")
-    .attr("r", 13)
-    .attr("transform", "translate(" + points[0][0] + ")");
+	.attr("r", 13)
+	.attr("transform", "translate(" + points[0][0] + ")");
 
 
 transition(0, console.log)();
 
 function transition(pathIndex, callback) {
-    return function() {
-        callback(pathIndex);
-        circle.transition()
-            .duration(10000)
-            .attrTween("transform", translateAlong(paths[pathIndex].node()))
-            .each("end", transition(++pathIndex % paths.length, callback));
-    }
+	return function() {
+		callback(pathIndex);
+		circle.transition()
+			.duration(10000)
+			.attrTween("transform", translateAlong(paths[pathIndex].node()))
+			.each("end", transition(++pathIndex % paths.length, callback));
+	}
 }
 
 // Returns an attrTween for translating along the specified path element.
 function translateAlong(path) {
-  var l = path.getTotalLength();
-  return function(d, i, a) {
-    return function(t) {
-      var p = path.getPointAtLength(t * l);
-      return "translate(" + p.x + "," + p.y + ")";
-    };
-  };
+	var l = path.getTotalLength();
+	return function(d, i, a) {
+		return function(t) {
+			var p = path.getPointAtLength(t * l);
+			return "translate(" + p.x + "," + p.y + ")";
+		};
+	};
 }
