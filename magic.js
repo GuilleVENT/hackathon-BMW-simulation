7b408f9b5c49ec96b594b4c6475bcf0da75abecd
felixschorer <felixschorer@users.noreply.github.com>
2017-11-18 14:46:28 +0100

    Broken...

--- magic.js
+++ magic.js
@@ -1,40 +1,59 @@
 var points = [
-  [480, 200],
-  [580, 400],
-  [680, 100],
-  [780, 300],
-  [180, 300],
-  [280, 100],
-  [380, 400]
+    [
+        [480, 200],
+        [580, 400],
+        [680, 100]
+    ],
+    [
+        [680, 100],
+        [780, 300],
+        [180, 300]
+    ],
+    [
+        [180, 300],
+        [280, 100],
+        [480, 200]
+    ]
 ];
 
 var svg = d3.select("body").append("svg")
     .attr("width", 960)
     .attr("height", 500);
 
-var path = svg.append("path")
-    .data([points])
-    .attr("d", d3.svg.line()
-    .tension(0) // Catmull–Rom
-    .interpolate("cardinal-closed"));
+var paths = points.map(points =>
+    svg.append("path")
+        .data([points])
+        .attr("d", d3.svg.line()
+            .tension(0) // Catmull–Rom
+            .interpolate("cardinal")
+        )
+);
 
-svg.selectAll(".point")
-    .data(points)
-  .enter().append("circle")
-    .attr("r", 4)
-    .attr("transform", function(d) { return "translate(" + d + ")"; });
+points.forEach(points => {
+    svg.selectAll(".point")
+        .data(points)
+        .enter().append("circle")
+        .attr("r", 4)
+        .attr("transform", function(d) { return "translate(" + d + ")"; });
+});
 
 var circle = svg.append("circle")
     .attr("r", 13)
-    .attr("transform", "translate(" + points[0] + ")");
+    .attr("transform", "translate(" + points[0][0] + ")");
 
-transition();
 
-function transition() {
-  circle.transition()
-      .duration(10000)
-      .attrTween("transform", translateAlong(path.node()))
-      .each("end", transition);
+transition(0)();
+
+function transition(pathIndex) {
+    return function() {
+        circle.transition()
+            .duration(10000)
+            .attrTween("transform", translateAlong(paths[pathIndex].node()))
+            .each("end", () => {
+                console.log(pathIndex);
+                transition(++pathIndex % paths.length);
+            });
+    }
 }
 
 // Returns an attrTween for translating along the specified path element.
